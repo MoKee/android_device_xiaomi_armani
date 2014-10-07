5ba9383fb5fc0a75052570593d0a124556545d01
martincz <martincz@mokeedev.com>
2014-10-07 22:58:08 +0800

    armani: code cleanup
    Change-Id: If7d05fb1b0eaf25bf7ebfd8a8fc709ce8eb4401c

--- mokee.mk
+++ mokee.mk
@@ -7,14 +7,8 @@ TARGET_SCREEN_WIDTH := 720
 
 TARGET_BOOTANIMATION_NAME := 720
 
-# Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mk/config/gsm.mk)
-
-$(call inherit-product, vendor/mk/config/cdma.mk)
-
-# Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mk/config/common_full.mk)
-
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 
 ## Device identifier. This must come after all inclusions
@@ -24,17 +18,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := HM1S
 PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_DEFAULT_LANGUAGE := zh
-PRODUCT_DEFAULT_REGION := CN
-
-#PRODUCT_LOCALES := zh_CN zh_TW en_US
-
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=HM1S \
     TARGET_DEVICE=armani \
     #BUILD_FINGERPRINT=Xiaomi/armani/armani:4.4.4/KTU84Q/JHCCNBF36.0:user/release-keys \
     #PRIVATE_BUILD_DESC="mk_armani-userdebug 4.4.4 KTU84Q JHCCNBF36.0 release-keys"
-
-#MK_NIGHTLY := true
-
