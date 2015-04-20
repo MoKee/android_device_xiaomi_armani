a9cd4928a6fc88eb534436b0f201d3aaed1d105d
martincz <martincz@mokeedev.com>
2015-04-20 10:44:14 +0800

    armani: mokee bringup
    Change-Id: Idb989a4bc0b7dbc485276ed891c94d7bf12c3fd0

--- mokee.mk
+++ mokee.mk
@@ -1,9 +1,9 @@
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := mk_armani
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
